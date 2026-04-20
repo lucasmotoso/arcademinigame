6b1b90933400f0f1a4f9de03e3e7a15bea02dffc
lucasmotoso <lucasmotoso@gmail.com>
2026-04-19 22:24:25 -0300

    chore: remove obsolete setup scripts and AI instruction texts

--- init-structure.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Initialize project structure for Arcade Mini Game 2.0
-
-mkdir -p src/{core,components,games/{pong,snake,sinuca},styles,utils}
-mkdir -p dist
-mkdir -p .vscode
-
-echo "✓ Project structure created successfully!"
--- setup.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-cd /d C:\Users\Lucas\Downloads\arcademinigame
-(
-  for %%D in ("src" "src\core" "src\components" "src\games" "src\games\pong" "src\games\snake" "src\games\sinuca" "src\styles" "src\utils" "dist" ".vscode") do (
-    if not exist "%%D" mkdir "%%D"
-  )
-  echo Directory structure created!
-) & pause
